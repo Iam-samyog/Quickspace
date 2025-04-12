07f0aab5df9291243359e05e4d68e58172f49616
Iam-samyog <msamyog37@gmail.com>
2025-04-12 18:55:07 +0545

    Added everything that is needed

--- src/Price.jsx
+++ src/Price.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Components/Navbar'
 import ContactUs from './Components/ContactUs'
 import PricingSection from './PricingSection'
 import QuickSpaceFooter from './QuickSpaceFooter'
 import StatsCard from './Components/StatsCard '
+import 'aos/dist/aos.css'; // Import AOS styles
+import AOS from 'aos'; // Import AOS script
 
 const Price = () => {
+    useEffect(() => {
+        // Initialize AOS
+        AOS.init({
+          duration: 1000, // Set animation duration (optional)
+          once: true, // Animation should only happen once (optional)
+        });
+      }, []);
   return (
     <>
     <div className='container mx-auto max-w-[1350px]'>
         <Navbar/>
-    <PricingSection/>
+    
+   <div data-aos="fade-up" ><PricingSection/></div>
      
      
     </div>
-    <StatsCard/>
-    <ContactUs/>
-    <div className='mt-20'>
+    <div data-aos="fade-left"><StatsCard/></div>
+    <div data-aos="fade-right"><ContactUs/></div>
+    <div className='mt-20' data-aos="fade-up">
         <QuickSpaceFooter/>
     </div>
     </>
